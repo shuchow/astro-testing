a17e2e32b6c3e70b4c0c8f4a32b8c486f0241d0e
Shu Chow <schow@rutgers.edu>
2025-02-20 00:45:41 -0800

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https//shu-astro.netify.app"
+    site: "https://shu-astro.netify.app"
 });
